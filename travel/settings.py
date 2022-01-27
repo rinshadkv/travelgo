8806001936b4636bf93ff4f4489e3afab5b58140
rinshadkv <muhammedrinshadkonu@gmail.com>
2022-01-27 16:13:42 +0530

    update1

--- travel/settings.py
+++ travel/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t9$qxq20g36wk82eb*qot0h9ldnmrbvrb^e)^_^&&&0u39h5v2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
